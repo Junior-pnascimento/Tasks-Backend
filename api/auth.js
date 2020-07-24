5875c168bdafe9ada0c0eb2b306dd229d094960f
Junior-pnascimento <junior.pnascimento@live.com>
2020-07-24 17:42:19 -0300

    Atualizado

--- api/auth.js
+++ api/auth.js
@@ -15,7 +15,7 @@ module.exports = app => {
         if (user) {
             bcrypt.compare(req.body.password, user.password, (err, isMatch) => {
                 if (err || !isMatch) {
-                    return res.status(401).send()
+                    return res.status(401).send('A senha informada é invalida!')
                 }
 
                 const payload = { id: user.id }
